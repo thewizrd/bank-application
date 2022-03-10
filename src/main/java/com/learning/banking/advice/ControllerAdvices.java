261ce0637377c89d7d7c4534d362fdcc1cf840ad
Dave Antoine <thewizrd.dev@gmail.com>
2022-03-10 16:03:23 -0500

    Advice: add controller advice for error message handling

---
+++ src/main/java/com/learning/banking/advice/ControllerAdvices.java
@@ -0,0 +1,75 @@
+package com.learning.banking.advice;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.learning.banking.exceptions.InsufficientFundsException;
+import com.learning.banking.exceptions.NoRecordsFoundException;
+import com.learning.banking.payload.response.ApiError;
+
+/**
+ * ControllerAdvices
+ *
+ * @author bryan
+ * @date Mar 10, 2022-3:34:42 PM
+ */
+@RestControllerAdvice
+public class ControllerAdvices extends ResponseEntityExceptionHandler {
+	@ExceptionHandler(NoRecordsFoundException.class)
+	public ResponseEntity<?> handleNoRecordsFoundException(NoRecordsFoundException e) {
+		ApiError apiError = new ApiError(HttpStatus.NOT_FOUND);
+		apiError.setMessage(e.getMessage());
+		return buildResponseEntity(apiError);
+	}
+	
+	@ExceptionHandler(InsufficientFundsException.class)
+	public ResponseEntity<?> handleInsufficientFundsException(InsufficientFundsException e) {
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST);
+		apiError.setMessage(e.getMessage());
+		return buildResponseEntity(apiError);
+	}
+	
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException e) {
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST);
+		apiError.setMessage("Validation Error");
+		apiError.addValidationErrors(e.getConstraintViolations());
+
+		return buildResponseEntity(apiError);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		// @Valid - validation failed
+		ApiError apiError = new ApiError(status);
+		apiError.setMessage("Validation Error");
+		apiError.addValidationErrors(ex.getFieldErrors());
+		apiError.addValidationObjectErrors(ex.getBindingResult().getGlobalErrors());
+		
+		return buildResponseEntity(apiError);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		ApiError apiError = new ApiError(status);
+		apiError.setMessage(ex.getMessage());
+		apiError.setDebugMessage(ex.getRequiredType().getName());
+		
+		return buildResponseEntity(apiError);
+	}
+	
+	private ResponseEntity<Object> buildResponseEntity(ApiError error) {
+		return ResponseEntity.status(error.getStatus()).body(error);
+	}
+}
