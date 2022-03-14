cb72791878867b46aa664d1d333ec04b114ccf12
Dave Antoine <thewizrd.dev@gmail.com>
2022-03-14 13:22:36 -0400

    UserDetailsImpl: disable user if status is DISABLED

--- src/main/java/com/learning/banking/security/service/UserDetailsImpl.java
+++ src/main/java/com/learning/banking/security/service/UserDetailsImpl.java
@@ -10,75 +10,65 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.learning.banking.entity.Customer;
+import com.learning.banking.enums.CustomerStatus;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = { "id" })
 public class UserDetailsImpl implements UserDetails {
 	private long id;
 	private String username;
 	@JsonIgnore // not include in response
 	private String password;
+	private CustomerStatus status;
 
 	private Collection<? extends GrantedAuthority> authorities;
-
-	private UserDetailsImpl(Long id, String username, String password,
-			Collection<? extends GrantedAuthority> authorities) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.authorities = authorities;
-	}
 
 	public static UserDetailsImpl build(Customer user) {
 		// GrantedAuthority, different role have different permission 权限
 		List<GrantedAuthority> authorities = user.getRoles().stream()
 				.map(role -> new SimpleGrantedAuthority(role.getRoleName().name())).collect(Collectors.toList());
-		return new UserDetailsImpl(user.getCustomerID(), user.getUsername(), user.getPassword(), authorities);
+		return new UserDetailsImpl(user.getCustomerID(), user.getUsername(), user.getPassword(), user.getStatus(), authorities);
 	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
 		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
 		return this.password;
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return this.username;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return true;
+		return status != CustomerStatus.DISABLED;
 	}
 }
